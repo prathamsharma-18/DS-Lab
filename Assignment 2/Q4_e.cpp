3e2409da2825dcfbee84be317384bbd744360700
prathamsharma-18 <psmahey4@gmail.com>
2025-08-28 16:15:05 +0530

    Q4_e.cpp

--- Assignment 2/Q4_e.cpp
+++ Assignment 2/Q4_e.cpp
@@ -1,26 +1,19 @@
 // Write a program to convert a character from uppercase to lowercase.
-#include <iostream>
+#include<iostream>
 using namespace std;
+int main(){
+    string str;
+    cout<<"enter a string: ";
+    getline(cin,str);
 
-int main()
-{
-  string str;
-  getline(cin, str);
-
-  int n = str.length();
-  bool flag = true;
-  for (int i = 0; i < n; i++)
-  {
-    if (str[i] >= 65 && str[i] < 91)
-    {
-      str[i] = str[i] + 32;
-      flag = false;
+    int n= str.length();
+    bool flag=true;
+    for(int i=0; i<n; i++){
+        if (str[i]>=65 && str[i]<=90) {
+            str[i]+=32;
+            flag=false;
+        }
     }
-  }
-  if (flag)
-    cout << "No uppercase found.";
-  else
-    cout << str;
-
-  return 0;
+    if (flag) cout<<"the string is already in lowercase: "<<str<<endl;
+    else cout<<"the string  in lowercase is "<<str<<endl;
 }

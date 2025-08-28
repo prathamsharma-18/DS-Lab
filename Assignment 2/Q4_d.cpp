6911cdd3676d96f7f711e67b66b6c1570e0469b5
prathamsharma-18 <psmahey4@gmail.com>
2025-08-28 16:14:09 +0530

    Q4_d.cpp

--- Assignment 2/Q4_d.cpp
+++ Assignment 2/Q4_d.cpp
@@ -1,26 +1,18 @@
 // Write a program to sort the strings in alphabetical order.
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int main()
-{
-  string str;
-  getline(cin, str);
+int main(){
+    string str;
+    cout<<"enter a string: ";
+    getline(cin,str);
 
-  int n = str.length();
+    int n = str.length();
 
-  for (int i = 0; i < n; i++)
-  {
-    for (int j = 0; j < n - i - 1; j++)
-    {
-      if (str[j] > str[j + 1])
-      {
-        swap(str[i], str[j]);
-      }
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n-i-1 ; j++){
+            if (str[j]>str[j+1]) swap(str[j],str[j+1]);
+        }
     }
-  }
-
-  cout << str;
-
-  return 0;
+    cout << str << endl;
 }

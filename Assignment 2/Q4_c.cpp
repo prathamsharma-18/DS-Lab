dc900fe3fdaa9a1db36ff296b41d210e39f083c4
prathamsharma-18 <psmahey4@gmail.com>
2025-08-28 16:13:06 +0530

    Q4_c.cpp

--- Assignment 2/Q4_c.cpp
+++ Assignment 2/Q4_c.cpp
@@ -1,27 +1,23 @@
 // Write a program to delete all the vowels from the string.
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-bool isVowel(char c)
-{
-  c = tolower(c);
-  return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+bool isVowel(char c){
+    c= tolower(c);
+    if (c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+    return true;
+    return false;
 }
-int main()
-{
-  string str;
-  getline(cin, str);
+int main(){
+    string str;
+    cout<<"enter a string: ";
+    getline(cin,str);
 
-  string result = "";
-
-  for (char ch : str)
-  {
-    if (!isVowel(ch))
-    {
-      result = result + ch;
+    string result;
+    for(char c : str){
+        if(!isVowel(c)){
+            result += c;
+        }
     }
-  }
-
-  cout << result;
-  return 0;
+    cout << result << endl;
 }
